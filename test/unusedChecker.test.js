1873741693b219f7059aca89f1ee3a33a1c323c5
Artur Delura <artur.delura@capita.co.uk>
2018-02-06 11:43:57 +0000

    Updated tests

--- test/unusedChecker.test.js
+++ test/unusedChecker.test.js
@@ -3,11 +3,6 @@ const unusedChecker = require("../lib");
 const path = require("path");
 
 describe("unusedChecker()", function () {
-    function test(sass, expectedUnused) {
-        const resolve = _ => sass;
-        assert.deepEqual(findUnused(["test.scss"], resolve), expectedUnused);
-    }
-
     it("should find unused variables", function (done) {
         const location = path.resolve("./fixtures/main.scss");
         const paths = [ path.resolve("./fixtures/deep/include") ];
@@ -16,10 +11,10 @@ describe("unusedChecker()", function () {
             .catch(function ({ unused }) {
                 assert.deepEqual(unused, [
                     'not-used',
-                    'bar',
-                    'btn-color',
-                    'color',
-                    'orb-t-less-than-pluto'
+                    'bar-not-used',
+                    'btn-color-not-used',
+                    'mixin-color-not-used',
+                    'less-than-pluto-not-used'
                 ]);
             })
             .then(done);
