7e400a904c16467afb2b2873e603c337d5e7d358
Artur Delura <artur.delura@capita.co.uk>
2018-02-07 11:05:56 +0000

    Some refactoring + configurable ingore + more tests

--- test/unusedChecker.test.js
+++ test/unusedChecker.test.js
@@ -2,24 +2,59 @@ const { assert } = require("chai");
 const unusedChecker = require("../lib");
 const path = require("path");
 
+const entryPoint = path.resolve("./fixtures/_main.scss");
+const includePaths = [ path.resolve("./fixtures/vendor") ];
+
 describe("unusedChecker()", function () {
-    it("should find unused variables", function (done) {
-        const entryPoint = path.resolve("./fixtures/main.scss");
-        const includePaths = [ path.resolve("./fixtures/deep/include") ];
+    it("should ignore mixins, functions, includes", function (done) {
+        unusedChecker(entryPoint, {
+            includePaths,
+            exclude: [
+                "includes",
+                "mixins",
+                "functions"
+            ]
+        }).catch(function ({ unused }) {
+            assert.deepEqual(unused, [
+                'bar-not-used',
+                'btn-color-not-used'
+            ]);
 
-        unusedChecker(entryPoint, includePaths)
-            .catch(function ({ unused }) {
-                unused = unused.map(variable => variable.name);
+            done();
+        });
+    });
 
-                assert.deepEqual(unused, [
-                    // 'not-used', // <-- defined in include paths
-                    'bar-not-used',
-                    'btn-color-not-used',
-                    // 'mixin-color-not-used', // <-- mixin
-                    // 'less-than-pluto-not-used' // <-- mixin
-                ]);
+    it("should log variables from exclude", function (done) {
+        unusedChecker(entryPoint, {
+            includePaths,
+            exclude: [
+                "mixins",
+                "functions"
+            ]
+        }).catch(function ({ unused }) {
+            assert.deepEqual(unused, [
+                'not-used', // <-- defined in include paths
+                'bar-not-used',
+                'btn-color-not-used'
+            ]);
 
-                done();
-            });
+            done();
+        });
+    });
+
+    it("should log variables from exclude", function (done) {
+        unusedChecker(entryPoint, {
+            includePaths,
+        }).catch(function ({ unused }) {
+            assert.deepEqual(unused, [
+                'not-used', // <-- defined in include paths
+                'bar-not-used',
+                'btn-color-not-used',
+                'mixin-color-not-used', // <-- mixin
+                'less-than-pluto-not-used' // <-- mixin
+            ]);
+
+            done();
+        });
     });
 });
